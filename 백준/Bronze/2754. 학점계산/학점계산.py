c96a248dca462846fe04d8008fd631890cb4cc27
GotPrgmer <55742497+GotPrgmer@users.noreply.github.com>
2022-06-16 23:29:04 +0900

    [Bronze V] Title: 학점계산, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,27 @@
+N = input()
+if N=='A+':
+    print('4.3')
+elif N=='A0': 
+    print('4.0')
+elif N=='A-':
+    print('3.7')
+elif N=='B+':
+    print('3.3')
+elif N=='B0':
+    print('3.0')
+elif N=='B-':
+    print('2.7')
+elif N=='C+':
+    print('2.3')
+elif N=='C0':
+    print('2.0')
+elif N=='C-':
+    print('1.7')
+elif N=='D+':
+    print('1.3')
+elif N=='D0':
+    print('1.0')
+elif N=='D-':
+    print('0.7')
+elif N=='F':
+    print('0.0')
