e2691b292350045ffc956d8625b5fc3fd09c3508
GotPrgmer <55742497+GotPrgmer@users.noreply.github.com>
2022-07-24 02:37:07 +0900

    [D2] Title: 달팽이 숫자, Time: 142 ms, Memory: 56,676 KB -BaekjoonHub

---
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -0,0 +1,54 @@
+testcase = int(input())
+
+for case in range(1,testcase + 1):
+    howbig = int(input())
+    answer = [[0]*howbig for i in range(howbig)]
+    for i in range(0,howbig):
+        answer[0][i] = i + 1
+        number = i +1
+    if howbig == 1:
+        print(f'#{case}')
+        print(1)
+        continue
+    cnt = 1 # 행인지 열인지
+    cnt_odd = howbig - 1 # for문 두개 거칠때마다 작아짐
+    cnt_even = 0 #for 문 하나 거칠때마다 커짐
+    fix1 = 1
+    fix2 = 1
+    for i in range(howbig-2,-1,-1):
+
+        if cnt % 2== 1:
+            for row in range(fix1,fix1 + i +1):
+                number += 1
+                answer[row][cnt_odd] = number
+
+            for col in range(fix1 - 1 + i ,fix1-2,-1):
+                number += 1
+                answer[cnt_odd][col] = number
+            cnt_odd -= 1
+            cnt += 1
+            fix1 += 1
+            if number == howbig * howbig:
+                break
+            else:
+                continue
+
+        elif cnt % 2 == 0: # cnt가 짝수일때
+            for row in range(fix2 + i,fix2-1,-1):
+                number += 1
+                answer[row][cnt_even] = number
+            cnt_even += 1
+            for col in range(fix2,fix2 + i+1):
+                number += 1
+                answer[cnt_even][col] = number
+            cnt += 1
+            fix2 += 1
+            if number == howbig * howbig:
+                break
+            else:
+                continue
+    print(f'#{case}')
+    for i in range(howbig):
+        for j in range(howbig):
+            print(answer[i][j],end=' ')
+        print()
