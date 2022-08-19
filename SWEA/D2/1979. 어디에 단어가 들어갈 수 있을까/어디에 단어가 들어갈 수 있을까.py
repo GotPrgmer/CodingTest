09e3826009b8cc0f3fb078219ab7773559094789
GotPrgmer <55742497+GotPrgmer@users.noreply.github.com>
2022-08-19 14:22:45 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 161 ms, Memory: 58,784 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,31 +1,53 @@
-line = int(input())
-for i in range(line):
-    rc, count = map(int,input().split())
-    Matrix = [[0]*rc for i in range(rc)]
-    answer = 0
+T = int(input())
+
+for tc in range(1, T + 1):
+    N,k = map(int,input().split())
+    Matrix = []
     cnt = 0
-    for j in range(rc):
-        row = list(map(int,input().split()))
-        Matrix[j]= row
+    word = 0
 
-    for k in range(rc):
-        for l in range(rc):
-            if Matrix[k][l]==1:
-                cnt+=1
-            else:
-                if cnt == count:
-                    answer +=1
-                else:
-                    cnt=0
-        for l in range(rc):
-            if Matrix[l][k]==1:
-                cnt+=1
-            else:
-                if cnt == count:
-                    answer +=1
-                else:
-                    cnt=0
-    print(answer)
+    for i in range(N):
+        Matrix += [[int(i) for i in input().split()]]
 
-        #가로에서 구해서 cnt에 누적시키고
-        #세로에서 구해서 cnt에 누적시킨다.
+    for row in range(0, N):
+        cnt = 0
+        for col in range(0, N):
+            #값이 1이면 cnt에 1을 더해주고
+            #값이 0이고 cnt가 k이면 word올려주고 cnt초기화
+            #값이 0이고 cnt가 k가 아니면 cnt초기화
+            if Matrix[row][col] == 1:
+                cnt += 1
+            elif cnt == k:
+                word += 1
+                cnt = 0
+            else:
+                cnt = 0
+        if cnt == k:
+            word += 1
+            cnt = 0
+        else:
+            cnt = 0
+
+
+    Matrix = list(zip(*Matrix))
+
+    for row in range(0, N):
+        cnt = 0
+        for col in range(0, N):
+            #값이 1이면 cnt에 1을 더해주고
+            #값이 0이고 cnt가 k이면 word올려주고 cnt초기화
+            #값이 0이고 cnt가 k가 아니면 cnt초기화
+            if Matrix[row][col] == 1:
+                cnt += 1
+            elif cnt == k:
+                word += 1
+                cnt = 0
+            else:
+                cnt = 0
+        if cnt == k:
+            word += 1
+            cnt = 0
+        else:
+            cnt = 0
+    print(f'#{tc} {word}')
+
