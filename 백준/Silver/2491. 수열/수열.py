d02d42721f7ccebe595185f3bff11838922ec9ef
GotPrgmer <55742497+GotPrgmer@users.noreply.github.com>
2022-08-31 02:59:12 +0900

    [Silver IV] Title: 수열, Time: 128 ms, Memory: 32404 KB -BaekjoonHub

--- 백준/Silver/2491. 수열/수열.py
+++ 백준/Silver/2491. 수열/수열.py
@@ -3,12 +3,10 @@ input = sys.stdin.readline
 
 Num = int(input())
 num_list = list(map(int,input().split()))
-ascending_num_list = sorted(num_list,reverse=True)
-descending_num_list = sorted(num_list)
 max_num = 1
 des_cnt = 1
 asc_cnt = 1
-
+#앞에서 부터 비교하면 앞 뒤를 비교해서 증
 for i in range(len(num_list)-1):
     if num_list[i] >= num_list[i+1]:
         des_cnt += 1
@@ -18,6 +16,7 @@ for i in range(len(num_list)-1):
         des_cnt = 1
 if max_num < des_cnt:
     max_num = des_cnt
+
 for i in range(len(num_list) - 1):
     if num_list[i] <= num_list[i+1]:
         asc_cnt += 1
@@ -28,10 +27,5 @@ for i in range(len(num_list) - 1):
 if max_num < asc_cnt:
     max_num = asc_cnt
 asc_cnt = 1
-
-
-
-
-
 
 print(max_num)
