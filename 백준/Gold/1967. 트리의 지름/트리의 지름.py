c6c9fa99a5d93e612f114fe082babae801ea62f5
MinJunKim <55742497+GotPrgmer@users.noreply.github.com>
2023-08-31 19:06:35 +0900

    [Gold IV] Title: 트리의 지름, Time: 84 ms, Memory: 35812 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -1,29 +1,15 @@
 import sys
-from collections import deque
+sys.setrecursionlimit(10**6)
 
 def input():
     return sys.stdin.readline().rstrip()
 
-def diameter(x,start_meter):
-    stack = deque([x])
-    visited = set()
-    first_meter = start_meter
-    first_node = tuple()
-    #루트에서 가장 먼 점 찾기
-    while stack:
-        meter, cur = stack.pop()
-
-        if cur in visited:
-            continue
-        visited.add(cur)
-        if first_meter < meter:
-            first_meter = meter
-            first_node = cur
-        for dest in tree[cur]:
-            if dest[1] not in visited:
-                stack.append((meter+dest[0],dest[1]))
-
-    return (first_meter,first_node)
+def diameter(node,length):
+    for dest in tree[node]:
+        #해당 노드가 visited에 없으면
+        if dist[dest[1]]==-1:
+            dist[dest[1]] = length + dest[0]
+            diameter(dest[1],dist[dest[1]])
 
 
 n = int(input())
@@ -31,12 +17,18 @@ n = int(input())
 tree = [[] for _ in range(n+1)]
 if n == 1:
     print(0)
-else:    
+else:
     for _ in range(n-1):
         parent, child, w = map(int,input().split())
         tree[parent].append((w,child))
         tree[child].append((w,parent))
-    first_ans = diameter((0,1),0)
-    print(int(diameter((0,first_ans[1]),first_ans[0])[0]))
+    dist = [-1]*(n+1)
+    dist[1] = 0
+    diameter(1,0)
+    first_node = dist.index(max(dist))
+    dist = [-1]*(n+1)
+    dist[first_node] = 0
+    diameter(first_node,0)
+    print(max(dist))
 
 
