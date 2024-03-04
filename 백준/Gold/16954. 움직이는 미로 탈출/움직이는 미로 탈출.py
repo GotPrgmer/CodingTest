eddf19ba1a5c90636e7105bdeb047a9c629cfcc7
MinJunKim <55742497+GotPrgmer@users.noreply.github.com>
2024-03-04 21:12:15 +0900

    [Gold III] Title: 움직이는 미로 탈출, Time: 68 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.py
+++ 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.py
@@ -1,43 +1,58 @@
 import sys
 from collections import deque
 
-graph = [[] for x in range(8)]
-walllist = []
-for i in range(8):
-    temp = sys.stdin.readline()
-    for j in range(8):
-        graph[i].append(".")
-        if temp[j] == "#":
-            walllist.append([i, j])
+def input():
+    return sys.stdin.readline().rstrip()
 
-# start: 7, 0 // end: 0, 7
-def bfs():
-    cnt = 0
-    q = deque([(7, 0)]) # x, y
+d_r = (0,-1,0,1,0,-1,1,1,-1)
+d_c = (0,0,1,0,-1,1,1,-1,-1)
+# 벽 set생성
+
+
+def sol():
+    ans = 0
+    wall = set()
+    visited = set()
+    q = deque([(7, 0)])
+
+    board = [list(input()) for _ in range(8)]
+    for r in range(8):
+        for c in range(8):
+            if board[r][c] == "#":
+                wall.add((r, c))
+    #bfs를 사용
     while q:
+        #현재 턴만틈 돌리기
         for _ in range(len(q)):
-            x, y = q.popleft()
-
-            if [x,y] in walllist:
+            r ,c = q.popleft()
+            if (r,c) in wall:
                 continue
-            if x == 0 or cnt == 9:
-                print(1)
-                return
-            adjlist = [[0, 0], [1, 0], [-1, 0], [0, -1], [0, 1], [-1, -1], [-1, 1], [1, -1], [1, 1]]
 
-            for addx, addy in adjlist:
-                nx = addx + x
-                ny = addy + y
-                if nx >= 0 and nx < 8 and ny >= 0 and ny < 8:
-                    if [nx, ny] not in walllist:
-                        q.append((nx, ny))
-        for i in range(len(walllist)):
-            walllist[i] = [walllist[i][0]+1, walllist[i][1]]
-        cnt+=1
+            if r == 0 and c == 7:
+                ans = 1
+                return ans
 
-    print(0)
+            #현재 위치에서 각 방향으로 가서 q에 넣기
+            for nt in range(9):
+                nt_r = r + d_r[nt]
+                nt_c = c + d_c[nt]
 
-if len(walllist) == 0:
-    print(1)
-else:
-    bfs()
+                if 0<=nt_r<8 and 0<=nt_c<8 and (nt_r,nt_c) not in visited and (nt_r,nt_c) not in wall:
+                    visited.add((nt_r,nt_c))
+                    q.append((nt_r,nt_c))
+        #다음턴을 위한 준비
+        if wall:
+            #wall이 없다면? 이대로 쭈루룩
+            #wall이 있다면? 방 초기화해서 갔던 곳 또가게 함.
+            visited = set()
+            nt_wall = set()
+            #벽 갱신
+            for w in wall:
+                if w[0] < 7:
+                    nt_wall.add((w[0]+1,w[1]))
+            wall = nt_wall
+    return ans
+
+
+
+print(sol())
