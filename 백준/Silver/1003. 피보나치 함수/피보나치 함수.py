8de49cff5d7fefcc9f1eee944a9c123b9d0c428e
MinJunKim <55742497+GotPrgmer@users.noreply.github.com>
2024-03-11 04:10:40 +0900

    [Silver III] Title: 피보나치 함수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -3,20 +3,6 @@ import sys
 def input():
     return sys.stdin.readline().rstrip()
 
-def fibonaci(x):
-    global cnt_1
-    global cnt_0
-    if memoi[x] != -1:
-        if x == 1:
-            cnt_1 += 1
-        elif x == 0:
-            cnt_0 += 1
-        return memoi[x]
-
-    else:
-        memoi[x] = fibonaci(x-2) + fibonaci(x-1)
-        return
-
 T = int(input())
 memoi = [[1,0],[0,1]]
 for _ in range(T):
