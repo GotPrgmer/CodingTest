532727c8e2de64caca7d4651616a044620b29304
MinJunKim <55742497+GotPrgmer@users.noreply.github.com>
2024-03-19 01:48:32 +0900

    [Gold III] Title: MST 게임, Time: 420 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/16202. MST 게임/MST 게임.py
+++ 백준/Gold/16202. MST 게임/MST 게임.py
@@ -1,42 +1,49 @@
 import sys
-input = sys.stdin.readline
 
+def input():
+    return sys.stdin.readline().rstrip()
 
-def find(x):
-    if parent[x] == x:
-        return x
-    parent[x] = find(parent[x])
+def find_parent(x,parent):
+    if parent[x] != x:
+        parent[x] =  find_parent(parent[x],parent)
     return parent[x]
 
-def union(a,b):
-    a = find(a)
-    b = find(b)
-
-    if level[a] >= level[b]:
-        parent[b] = a
-        if level[a] == level[b]:
-            level[a] += 1
+def union(parent, x,y):
+    x = find_parent(x,parent)
+    y = find_parent(y,parent)
+    if x < y:
+        parent[y] = x
     else:
-        parent[a] = b
+        parent[x] = y
 
-N, M, K = map(int, input().split())
-edges = []
-for i in range(1,M+1):
-    a, b = map(int, input().split())
-    edges.append((i,a,b))
-for i in range(K):
-    answer = 0
-    cnt = 0
-    parent = {i: i for i in range(1,N+1)}
-    level = {i: 0 for i in range(1,N+1)}
-    for edge in edges[i:]:
-        c, a, b = edge
-        if find(a) != find(b):
-            union(a,b)
-            cnt += 1
-            answer += c
-        if cnt == N-1:
-            print(answer, end = ' ')
+def sol():
+    ans_lst = []
+    for k in range(K):
+        parent = [i for i in range(N+1)]
+        score = 0
+        nth = 0
+        for e in edge[k:]:
+            cost, a, b = e
+            if find_parent(a,parent) != find_parent(b,parent):
+                union(parent,a,b)
+                nth += 1
+                score += cost
+            else:
+                continue
+        if nth == N - 1:
+            ans_lst.append(score)
+        else:
             break
-    else:
-        print(0, end=' ')
+    ans_lst.extend([0]*(K-len(ans_lst)))
+    return ans_lst
+
+
+N, M, K = map(int,input().split())
+
+
+edge = []
+for k in range(1,M+1):
+    x, y = map(int, input().split())
+    edge.append((k, x, y))
+
+print(*sol())
