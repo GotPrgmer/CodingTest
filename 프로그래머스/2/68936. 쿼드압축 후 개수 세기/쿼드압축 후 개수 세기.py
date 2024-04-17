691d98f564cef6b3cc83d5440eef63d2422ac1c9
MinJunKim <55742497+GotPrgmer@users.noreply.github.com>
2024-04-17 17:03:25 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 134.33 ms, Memory: 12.6 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
@@ -1,104 +1,30 @@
-def solution(arr):    
-    zero = 0
-    one = 0
-    def quad(r,c,l):
-        nonlocal zero
-        nonlocal one
-        if l == 0:
-            return
+one = 0
+zero = 0
 
-
-        start_r = r
-        start_c = c
-
-        #좌상
-        flag = 0
-        tmp = arr[start_r][start_c]
-        for r in range(start_r,start_r+l):
-            for c in range(start_c,start_c+l):
-                if arr[r][c] != tmp:
-                    flag = 1
-                    quad(start_r,start_c,l//2)
-                    break
-            if flag == 1:
-                break
-        if flag == 0:
-            if tmp == 1:
-                one += 1
-            else:
-                zero += 1
-
-        #우상
-        flag = 0
-        tmp = arr[start_r][start_c+l]
-        for r in range(start_r,start_r+l):
-            for c in range(start_c+l,start_c+2*l):
-                if arr[r][c] != tmp:
-                    flag = 1
-                    quad(start_r,start_c+l,l//2)
-                    break
-            if flag == 1:
-                break
-                    
-
-
-        if flag == 0:
-            if tmp == 1:
-                one += 1
-            else:
-                zero += 1
-
-        #좌하
-        flag = 0
-        tmp = arr[start_r+l][start_c]
-        for r in range(start_r+l,start_r+2*l):
-            for c in range(start_c,start_c+l):
-                if arr[r][c] != tmp:
-                    flag = 1
-                    quad(start_r+l,start_c,l//2)
-                    break
-            if flag == 1:
-                break
-        if flag == 0:
-            if tmp == 1:
-                one += 1
-            else:
-                zero += 1
-
-        #우하
-        flag = 0
-        tmp = arr[start_r+l][start_c+l]
-        for r in range(start_r+l,start_r+2*l):
-            for c in range(start_c+l,start_c+2*l):
-                if arr[r][c] != tmp:
-                    flag = 1
-                    quad(start_r+l,start_c+l,l//2)
-                    break
-            if flag == 1:
-                break
-
-        if flag == 0:
-            if tmp == 1:
-                one += 1
-            else:
-                zero += 1
-    
-    flag = 0
-    tmp = arr[0][0]
-    for r in range(0,0+len(arr)):
-        for c in range(0,0+len(arr)):
-            if arr[r][c] != tmp:
-                flag = 1
-                quad(0,0,len(arr)//2)
-                break
-        if flag == 1:
-            break
-        
-    if flag == 0:
-        if tmp == 1:
-            one += 1
-        else:
-            zero += 1
-
+def solution(arr):
+    global one
+    global zero
+    quad(arr,len(arr))
     return [zero,one]
-
+    
+def quad(arr,length):
+    global one
+    global zero
+    count = 0
+    for row in arr:
+        for each in row:
+            if each == 1:
+                count += 1
+                
+    if count == length**2:
+        one += 1
+    elif count == 0:
+        zero += 1
+    else:
+        quad([row[:length//2] for row in arr[:length//2] ],len(arr)//2)
+        quad([row[length//2:] for row in arr[:length//2] ],len(arr)//2)
+        quad([row[:length//2] for row in arr[length//2:] ],len(arr)//2)
+        quad([row[length//2:] for row in arr[length//2:] ],len(arr)//2)
+        
+        
+    
