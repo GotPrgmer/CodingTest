d691e2ed3768664585f90f479ba3d923712d21da
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2024-09-04 22:50:10 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,3 @@
+select MEMBER_ID,	MEMBER_NAME,	GENDER,	DATE_FORMAT(DATE_OF_BIRTH,'%Y-%m-%d') as DATE_OF_BIRTH
+from MEMBER_PROFILE
+where month(DATE_OF_BIRTH) = 3 and TLNO is not NULL and GENDER ='W';
