f875bf2f5ffe9b8233bea51af680c56366c2ce85
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2024-09-12 12:02:22 +0900

    [Gold III] Title: 양 구출 작전, Time: 512 ms, Memory: 93064 KB -BaekjoonHub

--- 백준/Gold/16437. 양 구출 작전/양 구출 작전.py
+++ 백준/Gold/16437. 양 구출 작전/양 구출 작전.py
@@ -1,34 +1,30 @@
 import sys
 sys.setrecursionlimit(10**6)
 from collections import defaultdict
-
 def input():
     return sys.stdin.readline().rstrip()
+
 tree = defaultdict(list)
 N = int(input())
-info = [['',0] for _ in range(N+1)]
-info[1] = ['root',0]
+info = [['',0] for _ in range(N+1) ]
+
 for i in range(2,N+1):
-    t, a, p = input().split()
+    t, a , p = input().split()
     a = int(a)
     p = int(p)
     tree[p].append(i)
     info[i][0] = t
     info[i][1] = a
-#dfs로 w이면 합계를 비교하고
-#s이면 합계를 더함
 def dfs(x):
+    #
     cnt = 0
-    #w이면
     for i in tree[x]:
         cnt += dfs(i)
-    if info[x][0] == 'S' and x != 1:
+    if info[x][0]=='W':
+            cnt -= info[x][1]
+    elif info[x][0]=='S':
         cnt += info[x][1]
-    elif info[x][0] == 'W':
-        cnt = cnt - info[x][1]
-    if cnt <= 0:
+    if cnt <0:
         cnt = 0
     return cnt
-
 print(dfs(1))
-
