fcabfd3ccd2bc44c33f3d84ea6912a0f93a4cd55
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2024-10-17 12:08:38 +0900

    [level 2] Title: 짝지어 제거하기, Time: 178.57 ms, Memory: 12.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,17 @@
+import sys
+sys.setrecursionlimit(10**6)
+def solution(s):
+    stack = []
+    for i in range(len(s)):
+        if len(stack) == 0:
+            stack.append(s[i])
+        elif stack[-1] == s[i]:
+            stack.pop()
+        else:
+            stack.append(s[i])
+            
+    if len(stack) == 0:
+        return 1
+    else:
+        return 0
+
