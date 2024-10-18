2b4b3ab830f1eff38e5fff89b227408acaa3c673
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2024-10-18 11:40:30 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,8 +1,3 @@
 def solution(absolutes, signs):
-    s = 0
-    for i in range(len(absolutes)):
-        if signs[i] == True:
-            s += absolutes[i]
-        else:
-            s -= absolutes[i]
-    return s
+    answer = 123456789
+    return answer
