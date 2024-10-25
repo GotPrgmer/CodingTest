44a7e5a0c018918db2b02948b0bfdace63d8ab10
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2024-10-25 16:15:30 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.py
@@ -0,0 +1,11 @@
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+n = int(input())
+for i in range(n,0,-1):
+    print(' '*(n-i) +'*'*(2*i-1))
+for i in range(2,n+1):
+    print(' '*(n-i) + '*'*(2*i-1))
