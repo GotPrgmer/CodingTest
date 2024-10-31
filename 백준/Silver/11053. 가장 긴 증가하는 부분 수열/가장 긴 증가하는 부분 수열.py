c3a623aae3a07d1ebb29d0d063c1d552a51eb966
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2024-10-31 17:33:29 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,22 +1,30 @@
-import bisect
 import sys
-
 def input():
     return sys.stdin.readline().rstrip()
 
 N = int(input())
-lst = list(map(int,input().split()))
-dp = [lst[0]]
+arr = list(map(int,input().split()))
 
-#현재 위치를 픽스시키고 과거의 항들을 조사해서 낮은 것들만 더함
-for e in range(N):
-    #마지막 항보다 크다면?
-    if dp[-1] < lst[e]:
-        dp.append(lst[e])
-    #현재 수열 안으로 들어갈래요
+def binarySearch(arr,num):
+    start = -1
+    end = len(arr)-1
+    while start+1<end:
+        m = (start+end)//2
+        if arr[m] < num:
+            start = m
+        else:
+            end = m
+    return end
+
+
+li = [arr[0]]
+for i in range(1,len(arr)):
+    if arr[i]>li[-1]:
+        li.append(arr[i])
     else:
-        idx = bisect.bisect_left(dp,lst[e])
-        dp[idx] = lst[e]
-
-
-print(len(dp))
+        # print(li)
+        idx = binarySearch(li,arr[i])
+        # print(idx)
+        li[idx] = arr[i]
+# print(li)
+print(len(li))
