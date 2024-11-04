dc76c1511213de8a28dbd49b57302f2138ecaaa9
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2024-11-05 02:42:20 +0900

    [Gold V] Title: 암호코드, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2011. 암호코드/암호코드.py
+++ 백준/Gold/2011. 암호코드/암호코드.py
@@ -9,12 +9,12 @@ dp = [0]*(l+1)
 if n[0] == 0:
     print('0')
 else:
-    n = [0]+n
+    n = n
     dp[0]=dp[1]=1
-    for i in range(2,l+1):
-        if n[i] >0:
-            dp[i] += dp[i-1]
+    for i in range(1,l):
+        if 9>=n[i] >0:
+            dp[i+1] += dp[i]
         tmp = n[i-1]*10 + n[i]
         if 10 <= tmp <= 26:
-            dp[i] += dp[i-2]
+            dp[i+1] += dp[i-1]
     print(dp[l]%1000000)
