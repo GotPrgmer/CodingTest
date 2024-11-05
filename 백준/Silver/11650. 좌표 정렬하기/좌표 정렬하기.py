5e495d391ad0942df48fab9f36b12bacd50184c5
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2024-11-05 15:43:05 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 344 ms, Memory: 51900 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,14 @@
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+N = int(input())
+lst = []
+for _ in range(N):
+    x, y = map(int,input().split())
+    lst.append((x,y))
+
+lst.sort(key=lambda x:(x[0],x[1]))
+for i in lst:
+    print(*i)
