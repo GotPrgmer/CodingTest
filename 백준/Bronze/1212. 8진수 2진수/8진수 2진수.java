fa50ef008e8d573cff8add22c0fb5cdcbd782d0a
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2024-11-26 16:50:50 +0900

    [Bronze II] Title: 8진수 2진수, Time: 1964 ms, Memory: 84936 KB -BaekjoonHub

---
+++ 백준/Bronze/1212. 8진수 2진수/8진수 2진수.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        BigInteger bi = new BigInteger(s, 8);
+        System.out.println(bi.toString(2));
+    }
+}
