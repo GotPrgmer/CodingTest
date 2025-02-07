53b806bab9b6ead7dad7bd63f382d1875bbe369b
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-02-07 18:38:52 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,13 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {   int cnt = 0;
+        while(a!=b){
+            a = a/2+a%2;
+            b = b/2+b%2;
+            cnt += 1;
+        }
+
+        return cnt;
+    }
+}
