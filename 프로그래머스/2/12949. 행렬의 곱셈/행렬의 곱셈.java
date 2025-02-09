349c64b0efbccbfd7b4e4016b6849e9a186216aa
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-02-09 15:42:11 +0900

    [level 2] Title: 행렬의 곱셈, Time: 10.24 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,18 +1,6 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        //axb bxc라고 하자, a와 b는 서로 곱셈이 가능하다.
-        int a = arr1.length;
-        int b = arr2.length;
-        int c = arr2[0].length;
-        int[][] answer = new int[a][c];
-            for(int cidx=0;cidx<c;cidx++){
-                for(int m=0;m<b;m++){
-                    for(int ridx =0;ridx<a;ridx++){
-                    answer[ridx][cidx] += arr1[ridx][m]*arr2[m][cidx];
-
-                }
-            }
-        }
+        int[][] answer = {};
         return answer;
     }
 }
