929d76e3b88113e988b8e1c7566847c35cd3f144
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-04-07 19:04:19 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.16 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,39 +1,24 @@
 class Solution {
     public String solution(String s) {
-        //공백 기준으로 나누기
-        String[] input = s.split(" ",-1);
         StringBuilder sb = new StringBuilder();
-        for(int i=0;i<input.length;i++){
-            if(i==input.length-1){
-                sb.append(process(input[i]));
-            }
-            else{
-                sb.append(process(input[i])+" ");
-            }
-            //공백이면 그대로 넣기
-            //단어끼리 하나로 모으기
-        }
-        return sb.toString();
-    
-    }
-    //첫머리 알파벳이면 대문자로 바꾸고 나머지 전부 소문자
-    public String process(String parseString){
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<parseString.length();i++){
-            if(!Character.isDigit(parseString.charAt(i))){
-                //두문자면 대문자로
-                if(i==0){
-                    sb.append(Character.toUpperCase(parseString.charAt(i)));
+        boolean isStart = true;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == ' ') {
+                sb.append(ch);
+                isStart = true;
+            } else {
+                if (isStart) {
+                    sb.append(Character.toUpperCase(ch));
+                } else {
+                    sb.append(Character.toLowerCase(ch));
                 }
-                else{
-                    sb.append(Character.toLowerCase(parseString.charAt(i)));
-                }
-            }
-            else{
-                sb.append(parseString.charAt(i));
+                isStart = false;
             }
         }
+
         return sb.toString();
     }
-               
 }
