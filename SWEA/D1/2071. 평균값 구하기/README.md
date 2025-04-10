f700221facb8402aafbbfb08d0002884d102e496
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-04-10 18:35:24 +0900

    [D1] Title: 평균값 구하기, Time: 79 ms, Memory: 24,832 KB -BaekjoonHub

---
+++ SWEA/D1/2071. 평균값 구하기/README.md
@@ -0,0 +1,15 @@
+# [D1] 평균값 구하기 - 2071 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QRnJqA5cDFAUq) 
+
+### 성능 요약
+
+메모리: 24,832 KB, 시간: 79 ms, 코드길이: 2,162 Bytes
+
+### 제출 일자
+
+2025-04-10 18:35
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D1/2071. 평균값 구하기/평균값 구하기.java
@@ -0,0 +1,50 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Solution {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine()); // 테스트 케이스 개수
+
+        for (int i = 0; i < T; i++) {
+            int sum = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            for (int j = 0; j < 10; j++) {
+                sum += Integer.parseInt(st.nextToken());
+            }
+
+            int avg = Math.round(sum / 10.0f); // 소수점 반올림
+            System.out.println("#" + (i + 1) + " " + avg);
+        }
+    }
+}
