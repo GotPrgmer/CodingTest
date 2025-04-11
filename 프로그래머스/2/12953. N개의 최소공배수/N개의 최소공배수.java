4746710070c312dbdbdd8eb4b1971b6eb071bab1
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-04-11 17:39:56 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.12 ms, Memory: 84.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] arr) {
+        Set<Integer> nums = new HashSet<>();
+        nums.add(arr[0]);
+        int cur = arr[0];
+        int gcd = 1;
+        for(int i=1;i<arr.length;i++){
+            if(!nums.contains(arr[i])){
+                gcd = gcd(cur,arr[i]);
+                // System.out.println("gcd"+gcd);
+                // System.out.println("oper"+cur+" "+arr[i]);
+                cur = gcd * cur/gcd * arr[i]/gcd;
+                // System.out.println("cur"+cur);
+                nums.add(arr[i]);
+            }
+        }
+
+        return cur;
+    }
+    public int gcd(int a, int b){
+        int big = Math.max(a,b);
+        int small = Math.min(a,b);
+        while(true){
+            int r = big%small;
+            
+            if(r==0){
+                break;
+            }
+            else{
+                big = small;
+                small = r;
+                continue;
+            }
+        }
+        return small;
+    }
+}
