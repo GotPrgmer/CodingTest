b1d4bd95d2b42d55c3419e598d0bd044d336de05
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-04-21 13:33:20 +0900

    [level 2] Title: 방문 길이, Time: 23.76 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,49 +1,49 @@
 import java.util.*;
 class Solution {
     public int solution(String dirs) {
-        int[] d_r = {1,0,-1,0};
-        int[] d_c = {0,1,0,-1};
-        int d = -1;
         int cur_r = 0;
         int cur_c = 0;
-        int n_r = 0;
-        int n_c = 0;
-        int ans = 0;
+        int[] d_r = {1,0,-1,0};
+        int[] d_c = {0,1,0,-1};
         Set<String> visited = new HashSet<>();
-        for(int i=0;i<dirs.length();i++){
-            System.out.println(cur_r + " " + cur_c);
-            
-            if(dirs.charAt(i) == 'U'){
-                System.out.println(dirs.charAt(i));
-                d = 0;
+        int ans = 0;
+        for(char c : dirs.toCharArray()){
+            System.out.println(cur_r+" "+cur_c);
+            int d=-1;
+            if(c=='U'){
+                d=0;
             }
-            else if(dirs.charAt(i) == 'R'){
-                System.out.println(dirs.charAt(i));
-
-                d = 1;
+            else if(c=='R'){
+                d=1;
             }
-            else if(dirs.charAt(i) == 'D'){
-                System.out.println(dirs.charAt(i));
-                d = 2;
+            else if(c=='D'){
+                d=2;
             }
             else{
-                System.out.println(dirs.charAt(i));
-                d = 3;
+                d=3;
             }
-            n_r = cur_r + d_r[d];
-            n_c = cur_c + d_c[d];
             
-            if(-5<=n_r && n_r<=5 && -5<=n_c && n_c<=5){
-                if(!visited.contains(cur_r+"."+cur_c+"."+n_r+"."+n_c)){
-                    visited.add(cur_r+"."+cur_c+"."+n_r+"."+n_c);
-                    visited.add(n_r+"."+n_c+"."+cur_r+"."+cur_c);
+            if(isArea(cur_r+d_r[d],cur_c+d_c[d])){
+                int n_r = cur_r + d_r[d];
+                int n_c = cur_c + d_c[d];
+                if(!visited.contains(cur_r+" "+cur_c+" "+n_r+" "+n_c)){
+                    visited.add(cur_r+" "+cur_c+" "+n_r+" "+n_c);
+                    visited.add(n_r+" "+n_c+" "+cur_r+" "+cur_c);
                     ans += 1;
                 }
                 cur_r = n_r;
                 cur_c = n_c;
             }
-            
         }
         return ans;
+    }
+    
+    public boolean isArea(int n_r, int n_c){
+            if(n_r>-6 && n_r<6 && n_c >-6 && n_c<6){
+                return true;
+            }
+        else{
+            return false;
+        }
     }
 }
