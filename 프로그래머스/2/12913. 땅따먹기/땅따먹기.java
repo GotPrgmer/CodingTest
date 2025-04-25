11d4027ae1cb68f300e1b6fb60b44041432ab99d
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-04-25 14:17:25 +0900

    [level 2] Title: 땅따먹기, Time: 19.04 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,27 +1,29 @@
+import java.util.*;
 class Solution {
     int solution(int[][] land) {
-        int answer = 0;
-        int n = land.length;
-
-        int[][] dp = new int[n][4];
+        int[][] dp = new int[land.length][4];
         for(int i=0;i<4;i++){
-            dp[0][i] = land[0][i];
+            dp[land.length-1][i] = land[land.length-1][i];
         }
-        for(int i=1;i<n;i++){
+        for(int i = land.length-1;i>0;i--){
             for(int j=0;j<4;j++){
                 for(int k=0;k<4;k++){
-                    //i행의 j열 갱신
                     if(j!=k){
-                        dp[i][j] = Math.max(dp[i][j],land[i][j]+dp[i-1][k]);
+                        dp[i-1][j] = Math.max(dp[i-1][j],land[i-1][j]+dp[i][k]);
                     }
                 }
-                
             }
+            
         }
+        int ans = 0;
         for(int i=0;i<4;i++){
-            answer = Math.max(answer,dp[n-1][i]);
+            ans = Math.max(ans,dp[0][i]);
         }
+        // System.out.println(ans);
 
-        return answer;
+
+        return ans;
     }
+
+    
 }
