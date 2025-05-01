7274e35fe9508913a6225e9c27073264dbae2ec2
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-05-01 11:06:34 +0900

    [level 3] Title: 단속카메라, Time: 11.62 ms, Memory: 60.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] routes) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>(){
+            @Override
+            public int compare(int[] a, int[] b){
+                return a[0]-b[0];
+            }
+        });
+        for(int i=0;i<routes.length;i++){
+            pq.add(routes[i]);
+        }
+        int[] first_car = pq.poll();
+        int cctv_loc = first_car[1];
+        int cnt = 1;
+        for(int i=1;i<routes.length;i++){
+            int[] cur = pq.poll();
+            //범위 이내
+            if(cctv_loc > cur[1]){
+                cctv_loc = cur[1];
+            }
+            else if(cctv_loc < cur[0]){
+                cnt += 1;
+                cctv_loc = cur[1];
+            }
+        }
+        return cnt;
+    }
+}
