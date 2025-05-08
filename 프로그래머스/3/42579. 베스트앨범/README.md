3b00d1d63f34c2bbc347111eb6a0efde1f00f585
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-05-08 12:39:30 +0900

    [level 3] Title: 베스트앨범, Time: 1.83 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 78.4 MB, 시간: 23.70 ms
+메모리: 75.6 MB, 시간: 1.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 11일 17:26:00
+2025년 05월 08일 12:39:25
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,34 +1,72 @@
 import java.util.*;
 class Solution {
+    
     public int[] solution(String[] genres, int[] plays) {
-        HashMap<String,Integer> genreCount = new HashMap<>();
-        HashMap<String,HashMap<Integer,Integer>> genrePlays = new HashMap<>();
-        
-        for(int idx=0;idx<plays.length;idx++){
-            String genre = genres[idx];
-            genreCount.put(genre,genreCount.getOrDefault(genre,0)+plays[idx]);
-            //genrePlays 갱신
-            HashMap<Integer,Integer> map = genrePlays.getOrDefault(genre,new HashMap<>());
-            map.put(idx,plays[idx]);
-            genrePlays.put(genre,map);
-            
+        Map<String,List<int[]>> map = new HashMap<>();
+        // System.out.println(genresByPlay(genres,plays).toString());
+        List<String[]> list = new ArrayList<>();
+        list = genresByPlay(genres,plays);
+        //장르별로 int[](횟수, 첨자) 저장
+        for(int i=0;i<genres.length;i++){
+            String genre = genres[i];
+            int play = plays[i];
+            List<int[]> tmp = map.getOrDefault(genre,new ArrayList<>());
+            tmp.add(new int[]{plays[i],i});
+            map.put(genre,tmp);
         }
-        List<String> keySet = new ArrayList(genreCount.keySet());
-        Collections.sort(keySet,(s1,s2)-> genreCount.get(s2)-genreCount.get(s1));
-        //장르 순서대로 최대 2개의 노래를 정답에 넣는다.
-        System.out.println(genreCount);
-        System.out.println(genrePlays);
-        ArrayList<Integer> answer = new ArrayList<>();
-        for(String genre:keySet){
-            HashMap<Integer,Integer> map = genrePlays.get(genre);
-            List<Integer> idxList = new ArrayList(map.keySet());
-            Collections.sort(idxList,(s1,s2)-> map.get(s2)-map.get(s1));
-            answer.add(idxList.get(0));
-            if(idxList.size()>=2){
-                answer.add(idxList.get(1));
+        // System.out.println(map);
+        
+        //장르별로 어레이 정렬
+        for(String s:map.keySet()){
+            Collections.sort(map.get(s),new Comparator<int[]>(){
+                @Override
+                public int compare(int[] i1, int[] i2){
+                    if(i1[0] != i2[0]){
+                        return i2[0] - i1[0];
+                    }
+                    else{
+                        return i1[1] - i2[1];
+                    }
+                }
+                
+            });
+        }
+        List<Integer> ans = new ArrayList<>();
+        //정렬에서 하나씩 빼서 장르별로 노래 인덱스를 answer에 추가
+        for(int i=0;i<list.size();i++){
+            String genre = list.get(i)[0];
+            //해당 장르 어레이를 돌면서 인덱스를 answer에 넣기
+            List<int[]> arr = map.get(genre);
+            for(int j=0;j<arr.size() && j<2;j++){
+                ans.add(arr.get(j)[1]);
             }
         }
-        System.out.println(answer);
-        return answer.stream().mapToInt(i -> i).toArray();
+        int[] answer = new int[ans.size()];
+        for(int i=0;i<ans.size();i++){
+            answer[i] = ans.get(i);
+        }
+        return answer;
+    }
+    public List<String[]> genresByPlay(String[] genres, int[] plays){
+        Map<String,Integer> map = new HashMap<>();
+        for(int i=0;i<genres.length;i++){
+            map.put(genres[i],map.getOrDefault(genres[i],0)+plays[i]);
+        }
+        //하나씩 리스트에 넣고 정렬
+        List<String[]> list = new ArrayList<>();
+        for(String s:map.keySet()){
+            list.add(new String[]{s,String.valueOf(map.get(s))});
+        }
+        Collections.sort(list,new Comparator<String[]>(){
+            @Override
+            public int compare(String[] s1,String[] s2){
+                return Integer.valueOf(s2[1])-Integer.valueOf(s1[1]);
+            }
+        });
+        // for(int i=0;i<list.size();i++){
+        //     System.out.println(list.get(i)[0]+list.get(i)[1]);
+        // }
+        
+        return list;
     }
 }
