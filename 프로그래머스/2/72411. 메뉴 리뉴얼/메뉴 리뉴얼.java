c959a764da9ebf6329e3c2fa209562fe81ab0ec1
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-05-24 08:15:48 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 6.82 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,60 +1,50 @@
 import java.util.*;
-
 class Solution {
     static Map<String,Integer> map;
     public String[] solution(String[] orders, int[] course) {
-        //문자열을 정렬
-        String[] order = new String[orders.length];
+        String[] order_list = new String[orders.length];
+        List<String> ans_list = new ArrayList<>();
         for(int i=0;i<orders.length;i++){
-            char[] c_array = orders[i].toCharArray();
-            Arrays.sort(c_array);
-            order[i] = String.valueOf(c_array);
+            char[] tmp = orders[i].toCharArray();
+            Arrays.sort(tmp);
+            order_list[i] = String.valueOf(tmp);
         }
-        //course에 있는 조합의 길이에 해당되는 조합들을 각 사용자한테서 구하고
-        //조합의 수중 가장 많은 수와 같이 매칭된 조합들을 추가하기
-        List<String> ans = new ArrayList<>();
         for(int i=0;i<course.length;i++){
             map = new HashMap<>();
-            StringBuilder sb = new StringBuilder(); 
-            for(int j=0;j<order.length;j++){
-             if(order[j].length()>=course[i]){
-                 combination(order[j],sb,0,0,course[i]);
-             }   
+            for(int j=0;j<order_list.length;j++){
+                if(order_list[j].length()>=course[i]){
+                    combi(order_list[j],new StringBuilder(),0,0,course[i]);
+                }
             }
-            // System.out.println(map);
-            //조합 매칭된 사람수 중에 최대값 구하기
             int max = 0;
             for(String s:map.keySet()){
                 max = Math.max(max,map.get(s));
             }
-            // System.out.println(max);
-            
-            //매칭 최대값과 같은 조합을 정답에 추가하기
             for(String s:map.keySet()){
-                if(max>=2 && max == map.get(s) && course[i] == s.length()){
-                    ans.add(s);
+                if(max>=2 && map.get(s)==max){
+                    ans_list.add(s);
                 }
             }
+            // System.out.println(map);
         }
-        Collections.sort(ans);
-        String[] answer = new String[ans.size()];
-        for(int i=0;i<ans.size();i++){
-            answer[i] = ans.get(i);
+        // System.out.println(Arrays.toString(order_list));
+        Collections.sort(ans_list);
+        // System.out.println(ans_list);
+        String[] answer = new String[ans_list.size()];
+        for(int i=0;i<answer.length;i++){
+            answer[i] = ans_list.get(i);
         }
         return answer;
     }
-    public void combination(String food_order,StringBuilder sb, int idx, int cnt, int n){
-        if(cnt==n){
+    public void combi(String order,StringBuilder sb, int idx, int cnt, int n){
+        if(cnt == n){
             map.put(sb.toString(),map.getOrDefault(sb.toString(),0)+1);
+            return;
         }
-        else{
-            for(int i=idx;i<food_order.length();i++){
-                sb.append(food_order.charAt(i));
-                combination(food_order,sb,i+1,cnt+1,n);
-                sb.delete(cnt,cnt+1);
-            }
+        for(int i=idx;i<order.length();i++){
+            sb.append(order.charAt(i));
+            combi(order,sb,i+1,cnt+1,n);
+            sb.delete(cnt,cnt+1);
         }
-        
     }
-    
 }
