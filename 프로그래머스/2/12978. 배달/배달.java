d3bd635e5f4abea45b9901f276cff7524a6b984c
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-05-24 09:34:11 +0900

    [level 2] Title: 배달, Time: 3.89 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,67 +1,70 @@
 import java.util.*;
 class Solution {
-    static List<List<int[]>> graph;
+    static List<List<Node>> graph; 
     public int solution(int N, int[][] road, int K) {
-        
         graph = new ArrayList<>();
         for(int i=0;i<N+1;i++){
             graph.add(new ArrayList<>());
         }
-        //그래프 초기화
         for(int i=0;i<road.length;i++){
             int a = road[i][0];
-            int b = road[i][1];
-            int t = road[i][2];
-            graph.get(a).add(new int[]{b,t});
-            graph.get(b).add(new int[]{a,t});
-            // System.out.println(graph.get(a));
+            int b= road[i][1];
+            int t= road[i][2];
+            graph.get(a).add(new Node(b,t));
+            graph.get(b).add(new Node(a,t));
         }
-        //다익스트라
+        // System.out.println(Arrays.toString(dijkstra(1,N)));
         int[] distance = dijkstra(1,N);
-        // System.out.println(Arrays.toString(distance));
         int answer = 0;
         for(int i=1;i<distance.length;i++){
             if(distance[i]<=K){
                 answer += 1;
             }
         }
+        
         return answer;
     }
-    
     public int[] dijkstra(int start,int N){
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<>(){
-            @Override
-            public int compare(int[] i1, int[] i2){
-                return i1[0]-i2[0];
-            }
-        });
         int[] distance = new int[N+1];
-        for(int i=1;i<distance.length;i++){
+        boolean[] visited = new boolean[N+1];
+        for(int i=0;i<distance.length;i++){
             distance[i] = Integer.MAX_VALUE;
         }
-        Set<Integer> visited = new HashSet<>();
-        distance[1] = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>(){
+            @Override
+            public int compare(int[] i1,int[] i2){
+                return i1[0] - i2[0];
+            }
+        });
+        distance[start] = 0;
         pq.add(new int[]{0,start});
-        while(visited.size()!=N && pq.size()!=0){
+        while(!pq.isEmpty()){
             int[] cur = pq.poll();
-            int cur_node = cur[1];
             int cur_time = cur[0];
-            // System.out.println(cur_node);
-            if(visited.contains(cur_node)){
+            int cur_node = cur[1];
+            if(visited[cur_node]){
                 continue;
             }
-            visited.add(cur_node);
-            //갱신
-            for(int[] i:graph.get(cur_node)){
-                // System.out.println(i[0]);
-                if(distance[i[0]] > cur_time + i[1] && !visited.contains(i[0])){
-                    // visited.add(i[0]);
-                    distance[i[0]] = cur_time + i[1];
-                    pq.add(new int[]{distance[i[0]],i[0]});
+            visited[cur_node] = true;
+            //현재 연결된 노드에서 주변 노드 업데이트 시키고
+            //업데이트 된 노드 pq에 추가
+            for(Node node:graph.get(cur_node)){
+                if(!visited[node.to] && distance[node.to]>cur_time+node.time){
+                    distance[node.to] = cur_time+node.time;
+                    pq.add(new int[]{distance[node.to],node.to});
                 }
             }
-            
         }
         return distance;
+        
+    }
+}
+
+class Node{
+    int to;
+    int time;
+    public Node(int to,int time){
+        this.to = to;
+        this.time = time;
     }
 }
