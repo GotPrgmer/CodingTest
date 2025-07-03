48d36ebb7b6df53678eb047fdf84946bd01aa5d3
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-07-04 00:50:28 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=python3) 
 
 ### 성능 요약
 
-메모리: 76.3 MB, 시간: 0.14 ms
+메모리: 9.11 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 16일 22:37:06
+2025년 07월 04일 00:50:23
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,16 +1,11 @@
-from itertools import combinations
 from collections import defaultdict
 def solution(clothes):
-    #clothes를 돌면서 각 카테고리에 해당하는 것들을 리스트에 추가한다.
-    category = defaultdict(list)
-    for c in clothes:
-        category[c[1]].append(c[0])
-    clothes = [i for i in category.keys()]
-    idxs = [i for i in range(len(clothes))]
-    ans = 0
-    tmp = 1
-    for i in idxs:
-        tmp *=len(category[clothes[idxs[i]]])+1
-    ans =tmp -1
-
+    cntDict = defaultdict(int)
+    for clothe in clothes:
+        cntDict[clothe[1]] +=1
+    ans = 1
+    for kind in cntDict.keys():
+        ans *= (cntDict[kind]+1)
+    ans -= 1
+    # print(cntDict)
     return ans
