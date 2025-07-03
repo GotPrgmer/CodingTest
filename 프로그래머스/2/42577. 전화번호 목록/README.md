303e4b76abc034f6804898e5e054283c1283a662
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-07-04 00:32:50 +0900

    [level 2] Title: 전화번호 목록, Time: 106.97 ms, Memory: 30 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=python3) 
 
 ### 성능 요약
 
-메모리: 183 MB, 시간: 263.11 ms
+메모리: 30 MB, 시간: 106.97 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 11일 12:17:24
+2025년 07월 04일 00:32:46
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,18 +1,6 @@
-import heapq
 def solution(phone_book):
-    heapq.heapify(phone_book)
-    answer = True
-    while phone_book:
-        cur = heapq.heappop(phone_book)
-        if not phone_book:
-            break
-        
-        elif len(cur)<=len(phone_book[0]):
-            #길이만큼 for문
-            compare = phone_book[0]
-            for i in range(len(cur)):
-                if cur[i] != compare[i]:
-                    break
-            else:
-                answer = False
-    return answer
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if(phone_book[i+1].find(phone_book[i])==0):
+            return False
+    return True
