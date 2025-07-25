ae9db0a183e360f1874443c41566adb9b3ecf309
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-07-26 03:29:29 +0900

    [Silver II] Title: N과 M (12), Time: 224 ms, Memory: 21268 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static Set<String> visited;
+    static List<String> answer;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] nAndM = br.readLine().split(" ");
+        String[] nList = br.readLine().split(" ");
+        visited = new HashSet<>();
+        answer = new ArrayList<>();
+        int n = Integer.parseInt(nAndM[0]);
+        int m = Integer.parseInt(nAndM[1]);
+        int[] nums = new int[nList.length];
+        for(int i=0;i<nums.length;i++){
+            nums[i] = Integer.parseInt(nList[i]);
+        }
+        Arrays.sort(nums);
+        backtrack(0,0,"",nums,m);
+        for(int i=0;i<answer.size();i++){
+            System.out.println(answer.get(i));
+        }
+    }
+    public static void backtrack(int cnt,int idx, String cur, int[] nums, int m){
+        if(cnt == m && !visited.contains(cur)){
+            visited.add(cur);
+            answer.add(cur);
+        }
+        else if(cnt != m){
+            for(int i=idx;i<nums.length;i++){
+                if(cur.isEmpty()){
+                    backtrack(cnt+1,i,String.valueOf(nums[i]),nums,m);
+                }
+                else{
+                    backtrack(cnt+1,i,cur+" "+nums[i],nums,m);
+                }
+            }
+        }
+
+
+    }
+}
