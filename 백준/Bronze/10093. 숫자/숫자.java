7a689a98b7b6f22cfc4fa2f6a962c7d8acb61e00
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-07-29 19:11:00 +0900

    [Bronze II] Title: 숫자, Time: 100 ms, Memory: 14188 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Integer.parseInt(st.nextToken());
+        long b = Integer.parseInt(st.nextToken());
+        long max = Math.max(a,b);
+        long min = Math.min(a,b);
+        if(max == min){
+            System.out.println("0");
+        }
+        else{
+            System.out.println(max - min -1);
+            StringBuilder sb = new StringBuilder();
+            for(long i=min+1;i<max;i++){
+                sb.append(i);
+                sb.append(" ");
+            }
+            System.out.println(sb);
+        }
+
+    }
+}
