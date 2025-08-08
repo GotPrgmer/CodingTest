1a90907d806956eb4e9fa3570a870575ff4ca5c8
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-08-08 19:58:45 +0900

    [Silver IV] Title: 스택, Time: 264 ms, Memory: 23972 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,25 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Main {
+    private static int M = 1000005;
+    private static int[] data = new int[M];
+    private static int pos = 0;
+
+    public static void push(int x){
+        data[pos] = x;
+        pos++;
+    }
+    public static int pop(){
+        pos -= 1;
+        return data[pos];
+    }
+    public static int top(){
+        return data[pos-1];
+    }
 
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int T = Integer.parseInt(br.readLine());
-        Stack<Integer> st = new Stack<>();
         for(int i=0;i<T;i++){
             String[] input = br.readLine().split(" ");
             String cmd = input[0];
             if(cmd.equals("push")){
-                st.add(Integer.parseInt(input[1]));
+                push(Integer.parseInt(input[1]));
             }
             else if(cmd.equals("pop")){
-                if(!st.isEmpty()){
-                    System.out.println(st.pop());
+                if(pos>0){
+                    System.out.println(pop());
                 }
                 else{
                     System.out.println(-1);
@@ -27,10 +39,10 @@ public class Main {
 
             }
             else if(cmd.equals("size")){
-                System.out.println(st.size());
+                System.out.println(pos);
             }
             else if(cmd.equals("empty")){
-                if(st.isEmpty()){
+                if(pos == 0){
                     System.out.println(1);
                 }
                 else{
@@ -38,8 +50,8 @@ public class Main {
                 }
             }
             else if(cmd.equals("top")){
-                if(!st.isEmpty()){
-                    System.out.println(st.peek());
+                if(pos>0){
+                    System.out.println(top());
                 }
                 else{
                     System.out.println(-1);
