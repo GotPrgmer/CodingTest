cc7124f74ac2ee51b0869c14dcce2b186c0c9774
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-08-21 15:55:08 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 256 ms, Memory: 24060 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -6,38 +6,20 @@ import java.util.List;
 import java.util.Stack;
 
 public class Main {
-    static class Cooperator{
-        int idx;
-        long num;
-        Cooperator(int idx, long num){
-            this.idx = idx;
-            this.num = num;
-        }
-    }
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int l = Integer.parseInt(br.readLine());
         List<Long> list = new ArrayList<>();
-        for(int i=0;i<l;i++){
-            list.add(Long.parseLong(br.readLine()));
-        }
-        Stack<Cooperator> st = new Stack<>();
+        Stack<Long> st = new Stack<>();
         long ans = 0;
         for(int i=l-1;i>-1;i--){
+            long h = Long.parseLong(br.readLine());
             //ans기록
-            if(!st.isEmpty()){
-                while(!st.isEmpty() && st.peek().num<list.get(i)){
-                    st.pop();
-                }
-
+            while(!st.isEmpty() && st.peek() <= h){
+                st.pop();
             }
-            if(!st.isEmpty()){
-                ans += st.peek().idx-i-1;
-            }
-            else{
-                ans += (l-1)-i;
-            }
-            st.add(new Cooperator(i,list.get(i)));
+            ans += st.size();
+            st.add(h);
         }
         System.out.println(ans);
     }
