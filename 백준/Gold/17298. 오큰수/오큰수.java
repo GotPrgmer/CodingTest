a034d6eb0f747393f255d62411350a724228cfe9
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-08-22 00:06:26 +0900

    [Gold IV] Title: 오큰수, Time: 848 ms, Memory: 144144 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,40 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
-        int[] result = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(br.readLine());
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+        int[] a = new int[l];
+        for(int i=0;i<l;i++){
+            a[i] =  Integer.parseInt(stringTokenizer.nextToken());
         }
-
-        Stack<Integer> stack = new Stack<>();
-
-        for (int i = 0; i < N; i++) {
-            while (!stack.isEmpty() && A[stack.peek()] < A[i]) {
-                result[stack.pop()] = A[i];
+        Stack<Integer> st = new Stack<>();
+        for(int i=0;i<l;i++){
+            while(!st.isEmpty() && a[i]>a[st.peek()]){
+                a[st.pop()] = a[i];
             }
-            stack.push(i);
+            st.add(i);
         }
-
-        // 스택에 남아 있는 인덱스에 대한 오큰수는 없으므로 -1로 유지됨
-        while (!stack.isEmpty()) {
-            result[stack.pop()] = -1;
+        while(!st.isEmpty()){
+            a[st.pop()] = -1;
         }
-
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            sb.append(result[i]).append(" ");
+        for(int i=0;i<a.length;i++){
+            sb.append(a[i]).append(' ');
         }
-
-        System.out.println(sb.toString().trim());
+        System.out.println(sb);
     }
 }
