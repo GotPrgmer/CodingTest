99f30bc815228869e22dfc159a5f700ac8fc37a1
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-09-16 12:22:11 +0900

    [Silver I] Title: 곱셈, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        System.out.println(cal(A,B,C));
+
+    }
+    static long cal(int A, int B, int C){
+        if(B==1){
+            return A%C;
+        }
+        long val = cal(A,B/2,C);
+        val = val*val%C;
+        if(B%2==0) return val;
+        return val*A%C;
+    }
+}
