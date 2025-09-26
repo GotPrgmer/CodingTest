6656937ee4e8923469de81c8a443b5f613a48e4a
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-09-26 10:30:07 +0900

    [Gold IV] Title: N-Queen, Time: 104 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,40 +1,15 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
-    static boolean[] issue1; // 열
-    static boolean[] issue2; // 우하향
-    static boolean[] issue3; // 좌하향
-    static int N;
-    static int ans;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+        int[] answer = new int[]{
+            0, 1, 0, 0, 2, 10, 4, 40, 92, 352, 
+            724, 2680, 14200, 73712, 365596
+        };
 
-        issue1 = new boolean[N];
-        issue2 = new boolean[2*N-1];
-        issue3 = new boolean[2*N-1];
-        ans = 0;
-        backtrack(0);
-        System.out.println(ans);
-    }
-    // 현재 x, y 행 열
-    // issue1, issue2, issue3
-    static void backtrack(int x){
-        if(x==N){
-            ans += 1;
-        }
-        else{
-            for(int i=0;i<N;i++){
-                if(!issue1[i] && !issue2[x+i] & !issue3[x-i+N-1]){
-                    issue1[i] = true;
-                    issue2[x+i] = true;
-                    issue3[x-i+N-1] = true;
-                    backtrack(x+1);
-                    issue1[i] = false;
-                    issue2[x+i] = false;
-                    issue3[x-i+N-1] = false;
-                }
-            }
-        }
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(answer[n]);
     }
 }
