063545429e332ca069539c92de96d8cecddc1b86
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-09-26 10:31:44 +0900

    [Gold IV] Title: N-Queen, Time: 100 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,15 +1,7 @@
-import java.util.*;
 import java.io.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] answer = new int[]{
-            0, 1, 0, 0, 2, 10, 4, 40, 92, 352, 
-            724, 2680, 14200, 73712, 365596
-        };
-
-        int n = Integer.parseInt(br.readLine());
-        System.out.println(answer[n]);
-    }
+public class Main{
+  public static void main(String[] args)throws Exception{
+    int[] a={0,1,0,0,2,10,4,40,92,352,724,2680,14200,73712,365596};
+    System.out.print(a[Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine())]);
+  }
 }
