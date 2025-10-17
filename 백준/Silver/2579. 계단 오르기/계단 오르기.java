188f8fd9d568f89f26f8252658c652e9cc61516d
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-10-17 11:51:18 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n+1];
+        for(int i=1;i<n+1;i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int[] dp = new int[n+1];
+        dp[1] = arr[1];
+
+        if(n>=2){
+            dp[2] = dp[1]+arr[2];
+            for(int i=3;i<n+1;i++){
+                dp[i] = Math.max(dp[i-2],dp[i-3]+arr[i-1])+arr[i];
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
