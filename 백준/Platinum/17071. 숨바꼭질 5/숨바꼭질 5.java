a3c04541469018f9d40190609d4cfee18317e183
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-11-11 10:33:17 +0900

    [Platinum V] Title: 숨바꼭질 5, Time: 240 ms, Memory: 72712 KB -BaekjoonHub

---
+++ 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static boolean[][] visited = new boolean[500000+1][2];
+    static int N;
+    static int K;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        System.out.println(bfs());
+    }
+    public static int bfs(){
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(N);
+        visited[N][0] = true;
+        int t = 0;
+        while(!q.isEmpty()){
+            int bro = K +t *(t+1)/2;
+            if(bro > 500000) return -1;
+            if(visited[bro][t%2]) return t;
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                int cur = q.poll();
+                for (int nxt : new int[]{cur - 1, cur + 1, cur * 2}) {
+                    if(nxt<0 || nxt > 500000) continue;
+                    if(visited[nxt][(t+1)%2]) continue;
+                    visited[nxt][(t+1)%2]=true;
+                    q.add(nxt);
+                }
+            }
+            t++;
+        }
+        return t;
+    }
+}
