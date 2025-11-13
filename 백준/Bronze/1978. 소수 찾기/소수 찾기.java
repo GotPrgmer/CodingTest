4c1c42ec1d86589b0c8623f65cc1759a2b5f0ad5
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2025-11-13 14:09:39 +0900

    [Bronze II] Title: 소수 찾기, Time: 104 ms, Memory: 14080 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,40 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
-
+import java.util.*;
+import java.io.*;
 public class Main {
-    public static void main(String[] args)throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        String[] input = br.readLine().split(" ");
-        int biggest = 0;
-        for(int idx=0;idx<input.length;idx++){
-            biggest = Math.max(biggest, Integer.parseInt(input[idx]));
-        }
-        List<Boolean> list = new LinkedList<>();
-        for(int idx=0;idx<biggest+1;idx++){
-            list.add(true);
-        }
-        list.set(0,false);
-        list.set(1, false);
-        for(int idx=0;idx<Math.sqrt(biggest)+1;idx++){
-            if(list.get(idx)){
-                for(int changeIdx=idx*2;changeIdx<biggest+1;changeIdx+=idx){
-                    list.set(changeIdx, false);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int ans = 0;
+        for (int t = 0; t < N; t++) {
+            int num = Integer.parseInt(st.nextToken());
+            if(num == 1) continue;
+            boolean isPrime = true;
+            for(int i=2;i*i<=num;i++){
+                if(num%i == 0){
+                    isPrime = false;
+                    break;
                 }
             }
-
-        }
-
-        int ans = 0;
-        for(int cntIdx=0;cntIdx<input.length;cntIdx++){
-            if(list.get(Integer.parseInt(input[cntIdx]))){
-                ans += 1;
+            if(isPrime){
+                ans++;
             }
         }
         System.out.println(ans);
-
     }
 }
