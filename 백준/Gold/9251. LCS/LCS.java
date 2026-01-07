7862e98c37187ee82e7eacaceaf06ed9175533c5
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2026-01-07 16:30:29 +0900

    [Gold V] Title: LCS, Time: 124 ms, Memory: 18864 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -6,20 +6,27 @@ import java.nio.Buffer;
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] s1 = br.readLine().toCharArray();
+        char[] s2 = br.readLine().toCharArray();
 
-        char[] a = br.readLine().toCharArray();
-        char[] b = br.readLine().toCharArray();
+        //dp[i][j]는 i개, j개 문자열에서 LCS
+        //i번째와 j번째가 같음
+        // dp[i][j] = dp[i-1][j-1] + 1;
+
+        //i번째는 LCS에 안들어감
+        //j번째는 들어갈지 안들어갈 지 모름
+        //그러면 dp[i-1][j]로 구하면 됨
+
+        //j번째가 LCS에 안들어감
+        //dp[i][j-1]
+
         int[][] dp = new int[1001][1001];
-        for(int i=0;i<a.length;i++){
-            for(int j=0;j<b.length;j++){
-                if(a[i] == b[j]){
-                    dp[i+1][j+1] = dp[i][j]+1;
-                }
-                else{
-                    dp[i+1][j+1] = Math.max(dp[i][j+1], dp[i+1][j]);
-                }
+        for(int i=0;i<s1.length;i++){
+            for(int j=0;j<s2.length;j++){
+                if(s1[i] == s2[j]) dp[i+1][j+1] = dp[i][j] + 1;
+                dp[i + 1][j + 1] = Math.max(dp[i + 1][j + 1], Math.max(dp[i][j + 1], dp[i + 1][j]));
             }
         }
-        System.out.println(dp[a.length][b.length]);
+        System.out.println(dp[s1.length][s2.length]);
     }
 }
