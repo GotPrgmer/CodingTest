b949eb93c876e2dba28c52946fe3a79723838790
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2026-02-05 19:37:47 +0900

    [Silver V] Title: 방 번호, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -1,25 +1,32 @@
 import java.io.BufferedReader;                                                                            
 import java.io.IOException;                                                                               
-import java.io.InputStreamReader;                                                                         
-import java.util.*;                                                                                       
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.*;
                                                                                                           
 public class Main {
     public static void main(String[] args) throws IOException {
-        int[] cnt = new int[10];
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));                         
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        String N = st.nextToken();
-        for(char c:N.toCharArray()){
-            cnt[(int)c-'0'] += 1;
-        }
-        int sixAndNine = (cnt[6]+cnt[9])/2 + (cnt[6]+cnt[9])%2;
-        int max = 0;
-        for(int i=0;i<9;i++){
-            if(i!=6){
-                max = Math.max(max,cnt[i]);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] roomNum = br.readLine().toCharArray();
+        int[] cnt = new int[9];
+        for (char c : roomNum) {
+            if(c=='9'){
+                cnt[6] += 1;
+            }
+            else{
+                cnt[c-'0'] += 1;
             }
         }
-        System.out.println(Math.max(max,sixAndNine));
+        int ans = 0;
+        for(int i=0;i<9;i++){
+            if(i==6){
+                ans = Math.max(ans, cnt[i] / 2 + cnt[i] % 2);
+            }
+            else{
+                ans = Math.max(ans, cnt[i]);
+            }
+        }
+        System.out.println(ans);
+
     }
-}                                                                                              
-                                                                                                          
+}
