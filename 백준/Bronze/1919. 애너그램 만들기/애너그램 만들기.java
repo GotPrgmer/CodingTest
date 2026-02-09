bc78850ccefc14c362413f60167f068253d8be0c
KIM MINJUN <55742497+GotPrgmer@users.noreply.github.com>
2026-02-09 20:51:43 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 96 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -6,27 +6,23 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int[] cnt = new int[26];
-        String first = st.nextToken();
-        for(char c:first.toCharArray()){
-            cnt[(int)c - 97] += 1;
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+        System.out.println(enigma(s1, s2));
+    }
+    private static int enigma(String s1, String s2){
+        int[] cnt1 = new int[26];
+        int[] cnt2 = new int[26];
+        for (char c : s1.toCharArray()) {
+            cnt1[c-'a'] += 1;
         }
-        st = new StringTokenizer(br.readLine());
-        String second = st.nextToken();
-        for(char c:second.toCharArray()){
-            cnt[(int)c - 97] -= 1;
+        for (char c : s2.toCharArray()) {
+            cnt2[c-'a'] += 1;
         }
         int ans = 0;
         for(int i=0;i<26;i++){
-            if(cnt[i] > 0){
-                ans += cnt[i];
-            }
-            else if(cnt[i] < 0){
-                ans += -cnt[i];
-            }
+            ans += Math.abs(cnt1[i] - cnt2[i]);
         }
-        System.out.println(ans);
+        return ans;
     }
 }
